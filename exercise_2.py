ea4ccef326ef40be85da63f2d0bf0f51f84eaf4d
Nadezda Fegele <nadezda.fegele@gmail.com>
2021-10-07 23:17:21 +0300

    edited exercise 2

--- exercise_2.py
+++ exercise_2.py
@@ -3,9 +3,9 @@
 
 with open("text.txt", 'r') as file:
     text = file.readlines()
-    lines_count = len(text)
-    print(f'The file contains {lines_count} lines.')
-    for index, line in enumerate(text):
-        words = line.split()
-        words_count = len(words)
-        print(f"Line #{index + 1}: {words_count} words")
+lines_count = len(text)
+print(f'The file contains {lines_count} lines.')
+for index, line in enumerate(text):
+    words = line.split()
+    words_count = len(words)
+    print(f"Line #{index + 1}: {words_count} words")
